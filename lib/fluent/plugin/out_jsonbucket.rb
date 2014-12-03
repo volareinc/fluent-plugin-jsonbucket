9bcf9cb4fa83c48b405f8bbe7e14f2ffafeefb16
polidog <polidogs@gmail.com>
2014-12-03 16:22:15 +0900

    エンコーディングを強制指定できるように拡張

--- lib/fluent/plugin/out_jsonbucket.rb
+++ lib/fluent/plugin/out_jsonbucket.rb
@@ -3,6 +3,7 @@ module Fluent
         Fluent::Plugin.register_output('jsonbucket', self)
         config_param :output_tag, :string, :default => nil
         config_param :json_key, :string, :default => 'json'
+        config_param :force_encoding, :string, :default => nil
 
         def configure(conf)
             super
@@ -12,11 +13,21 @@ module Fluent
         end
 
         def emit(tag, es, chain)
+            record = set_encoding(record) if @force_encoding
             es.each {|time,record|
                 chain.next
                 bucket = {@json_key => record.to_json}
                 Fluent::Engine.emit(@output_tag, time, bucket)
             }
-        end 
+        end
+
+        def set_encoding(record)
+          record.each_pair { |k, v|
+            if v.is_a?(String)
+              v.force_encoding(@char_encoding)
+            end
+          }
+        end
+
     end
 end
